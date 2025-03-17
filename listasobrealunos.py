e610ca133d4596fd8e1fe14e6866ecee6569988c
Satiro07 <jardelsatiro57@gmail.com>
2025-03-16 22:06:40 -0300

    Update listasobrealunos.py

--- listasobrealunos.py
+++ listasobrealunos.py
@@ -9,6 +9,7 @@
 def menu():
     print('1. adicionar aluno')
     print('2. remover aluno')
+    print('3. boletim')
 
 lista = []
 while True:
@@ -19,26 +20,52 @@ while True:
         while continuar == 's':
             nome = input('Digite o nome do aluno: ').strip()
             matricula = int(input('Digite a matricula do aluno: '))
-            lista.append([nome, matricula])
-            continuar = input('deseja continuar? [s/n] ')
+            materia_nota = []
+            materia_nota.append(nome)
+            materia_nota.append(matricula)
+            cont = 's'
+            while cont == 's':
+                mat = []
+                materia = input('Nome da matéria: ')
+                print('Digite as notas [digite um número menor de 0 ou maior de 10 para sair]')
+                nota = 1
+                mat.append(materia)
+                while 0 <= nota <= 10:
+                    nota = int(input('Digite a nota:'))
+                    mat.append(nota)
+                materia_nota.append(mat)
+                cont = input('deseja adicionar outra máteria? [s/n] ')
+            lista.append(materia_nota)
+            continuar = input('deseja adicionar outro aluno? [s/n] ')
+
     elif esc == '2':
         remover = int(input('Digite a matricula do aluno que deseja remover: '))
         for i in range(0, len(lista)):
             if remover == lista[i][1]:
                 lista.remove(lista[i])
-                break    
+                break   
+
+    elif esc == '3':
+        for aluno in lista:
+            print(aluno)
+
     else:
         print(lista)
+        print(len(lista))
 
 
-# nome = 'jose'
-# mat = 120
+
 # notas = []
+# notasa = 'matematica: 10'
 
-# notas.append('Matematica : 10')
-# lista.append([nome, mat, notas])
+# materia_nota = []
 # nome1 = 'jose'
 # mat1 = 120
-# lista.append([nome1, mat1])
+# materia_nota.append(notasa)
+# lista.append([nome1, mat1, materia_nota])
+
+
+
+
 # print(lista)
 # print(len(lista))

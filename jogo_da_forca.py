d3527bf5c2d9a9f6b735b9f89e5ef4de2390f379
Satiro07 <jardelsatiro57@gmail.com>
2025-03-22 10:07:19 -0300

    Update jogo_da_forca.py

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -7,24 +7,41 @@ tentativas = 6
 palavra = ''
 print('_' * quantidade_letras)
 quant2 = quantidade_letras
-
+palavra_cons = ''
+cont = 4
+palavranova = ''
+# print(palavra_cons.replace('b', sorteio_palavra[cont]))
 while True:
+    palavra_aux = ''
     while quant2:
         palavra += '_'
         quant2 -= 1
     palavra = palavra
     letra_usuario = input('Digite uma letra: ')
-    certo = 0
+    
     veri = quantidade_letras
     for i in range(0, veri):
         if letra_usuario == sorteio_palavra[i]:
             print('contem a letra')
-            palavra = letra_usuario + palavra
-            certo += 1
-        else:
-            palavra = '_' + palavra
+            
+            palavra_aux += letra_usuario
 
-    print(palavra)
-    if certo == 0:
-        print('não tem a palavra')
-        break
+        else:
+            palavra_aux += '_'
+
+    
+    if cont >= 2:
+        for i in range(0, veri):
+            if palavra_aux[i] != palavra_cons[i]:
+                palavranova.replace(palavra_aux[i],palavra_cons[i]) 
+            else:
+                palavranova.replace(palavra_aux[i],palavra_cons[i]) 
+    else:
+        palavranova = palavra_aux
+        palavra_cons = palavra_aux
+    
+    
+    print(palavra_aux)
+    print(palavra_cons)
+    print(palavranova)
+    cont += 1

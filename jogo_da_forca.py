4134e8f2e292fd02fc8a6f4e937e7341226e3d1d
Satiro07 <jardelsatiro57@gmail.com>
2025-03-21 21:01:32 -0300

    Update jogo_da_forca.py

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -4,22 +4,29 @@ sorteio_palavra = choice(palavras)
 quantidade_letras = len(sorteio_palavra)
 print(sorteio_palavra)
 tentativas = 6
-quantidade_letras2 = quantidade_letras
-while True:
-    letra = ''
-    barra = '_'
-    while quantidade_letras2:
-        print(barra, end=' ')
-        quantidade_letras2 -= 1
-    print()
-    letra_usuario = input('Digite uma letra: ')
-    certo = 0
-    veri = quantidade_letras
-    for i in range(0, veri):
-        if letra_usuario == sorteio_palavra[i]:
-            print('contem a palavra')
-            certo += 1
+palavra = ''
+print('_ ' * quantidade_letras)
+quant2 = quantidade_letras
+while quant2:
+    palavra += '_'
+    quant2 -= 1
+c = 1
+palavra[c] = 'a'
+print(palavra)
+# while True:
+    
+#     letra_usuario = input('Digite uma letra: ')
+#     certo = 0
+#     veri = quantidade_letras
+#     for i in range(0, veri):
+#         if letra_usuario == sorteio_palavra[i]:
+#             print('contem a letra')
+#             palavra[i] = letra_usuario
+#             certo += 1
+#         else:
+#             palavra += '_ '
 
-    if certo == 0:
-        print('não tem a palavra')
-        break
+#     print(palavra)
+#     if certo == 0:
+#         print('não tem a palavra')
+#         break

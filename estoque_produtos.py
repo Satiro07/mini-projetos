cd48b44a3cff568ad72d4ad5bc3e961d30831ba9
Satiro07 <jardelsatiro57@gmail.com>
2025-03-26 15:39:38 -0300

    Update estoque_produtos.py

--- estoque_produtos.py
+++ estoque_produtos.py
@@ -6,15 +6,8 @@ def menu():
     print('4 - Mostrar estoque')
     print('5 - Sair')
 
-def add_produto(quantidade, preco):
-    lista = []
-    lista.append([quantidade, preco])
-    return lista
-
 dic = {}
-
 while True:
-    geral = []
     menu()
     escolha = input('Escolha uma opção: ')
     if escolha == '1':
@@ -25,11 +18,33 @@ while True:
             quantidade = int(input('Quantidade: '))
             preco = float(input('Preço unitário: '))
             preco_total = quantidade * preco
-            dic[nome_produto] = quantidade, preco_total
-            continuar = input('Deseja adicionar mais produtos? [s/n]').lower()
+            dic[nome_produto] = [quantidade, preco_total, preco]
+            continuar = input('Deseja adicionar mais produtos? [s/n] ').lower()
+    elif escolha == '2':
+        nome = input('Nome do produto para atualizar: ')
+        if nome in dic:
+            nova_quant = int(input('Nova quantidade: '))
+            for k, v in dic.items():
+                if k == nome:
+                    v[0] = nova_quant
+                    v[1] = v[0] * v[2]
+        else:
+            print('Produto não encontrado! Verifique acentos!!')
+    elif escolha == '3':
+        nome = input('Nome do produto que deseja remover: ')
+        if nome in dic:
+            del dic[nome]
+    elif escolha == '4':
+        if len(dic) != 0:
+            for k, v in dic.items():
+                print(f'{k} - Quantidade: {v[0]} - Preço total: R${v[1]:.2f}')
+        else:
+            print('Você precisa adicionar produtos primeiro!')
+            
+    elif escolha == '5':
         break
-for k, v in dic.items():
-    print(f'{k} - Quantidade {v[0]} - Preço total {v[1]:.2f}')
+    else:
+        print('Escolha uma opção válida!')
     
 
 # \n

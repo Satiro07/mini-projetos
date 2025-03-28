8e7f5eda3cfd9149770500506832b005627b0e61
Satiro07 <jardelsatiro57@gmail.com>
2025-03-27 22:17:22 -0300

    Update anki.py

--- anki.py
+++ anki.py
@@ -8,9 +8,11 @@ def menu():
 
 
 
+lista = []
 dic = {}
-
 while True:
+    
+    lista = []
     menu()
     escolha = input('Escolha uma opção: ')
     if escolha == '1':
@@ -21,3 +23,25 @@ while True:
         print('Nome baralhos: ')
         for k in dic.keys():
             print(k)
+        escolha_baralho = input('Nome do baralho que deseja adicionar cartas: ')
+        if escolha_baralho in dic:
+            frente = input('Pergunta: ')
+            verso = input('Resposta: ')
+            dic1 = {}
+            for k in dic.keys():
+                if escolha_baralho == k:
+                    lista.append([frente, verso])
+                    dic = lista
+                    
+        else:
+            print('Baralho não encontrado')
+    elif escolha == '3':
+        c = 0
+        c1 = 1
+        for pergunta in lista:
+            for valor in pergunta.values():
+                for v in valor.values():
+                    print(f'Pergunta: {v[c]}, Resposta: {v[c1]}')
+                    c += 2
+                    c1 += 2
+    print(dic)

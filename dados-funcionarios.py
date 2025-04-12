d7b65c1e8c6dfb9dc85cd58133b7594568df96dc
Satiro07 <jardelsatiro57@gmail.com>
2025-04-11 22:30:37 -0300

    Update dados-funcionarios.py

--- dados-funcionarios.py
+++ dados-funcionarios.py
@@ -6,9 +6,17 @@ def menu():
 
 def adicionar_funcionario():
     func = {}
-    func['nome'] = input('Nome do funcionario: ')
-    func['cargo'] = input('Cargo do funcionario: ')
-    print(func)
+    sair = 's'
+    while sair != 'n':
+        func['nome'] = input('Nome do funcionario: ')
+        func['cargo'] = input('Cargo do funcionario: ')
+        funcionarios.append([func])
+        sair = input('Deseja adicionar outro funcionario? [s/n]').lower()
+
+def exibir_funcionarios():
+    for funcionario in funcionarios[0]:
+        for k, v in funcionario.items():
+            print(f'{k} = {v}')
 funcionarios = []
 
 
@@ -17,3 +25,6 @@ while True:
     escolha = int(input('Escolha uma opção: '))
     if escolha == 1:
         adicionar_funcionario()
+        print(funcionarios)
+    elif escolha == 2:
+        exibir_funcionarios()

008c28a4e1bc808aaf13458ea5b126d40b0c4b15
Satiro07 <jardelsatiro57@gmail.com>
2025-04-13 18:20:18 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,65 +1,65 @@
-# geral = []
-# produto = []
-# prod = 'feijao'
-# produto.append(prod)
-# geral.append(produto)
+# # geral = []
+# # produto = []
+# # prod = 'feijao'
+# # produto.append(prod)
+# # geral.append(produto)
 
-# tipos = []
-# tipo = 'kilo'
-# tipos.append(tipo)
-# geral.append(tipos)
+# # tipos = []
+# # tipo = 'kilo'
+# # tipos.append(tipo)
+# # geral.append(tipos)
 
-# valor = []
-# val = 5
-# valor.append(val)
-# geral.append(valor)
-# ind = 0 # nome produtos
-# ind1 = 1 # valores
-# ind2 = 2 # tipos
-# print(len(geral))
-# print(geral)
-
-
-# geral = [[5, 4], [4], [3]]
-# geral[0][1] = 3
-# print(geral)
+# # valor = []
+# # val = 5
+# # valor.append(val)
+# # geral.append(valor)
+# # ind = 0 # nome produtos
+# # ind1 = 1 # valores
+# # ind2 = 2 # tipos
+# # print(len(geral))
+# # print(geral)
 
 
-letras = []
+# # geral = [[5, 4], [4], [3]]
+# # geral[0][1] = 3
+# # print(geral)
 
-palavra = ['ninho']
-cont = len(palavra[0])
-quant = len(palavra[0])
-for i in range(0, quant):
-    letras.append('_')
-while True:
-    c = 0
-    letra = input('Letra: ')
+
+# letras = []
+
+# palavra = ['ninho']
+# cont = len(palavra[0])
+# quant = len(palavra[0])
+# for i in range(0, quant):
+#     letras.append('_')
+# while True:
+#     c = 0
+#     letra = input('Letra: ')
     
-    if letra not in palavra[0]:
-        print('erro')
+#     if letra not in palavra[0]:
+#         print('erro')
 
-    elif letra in letras:
-        print('Letra ja adicionada')
+#     elif letra in letras:
+#         print('Letra ja adicionada')
 
-    else:
-        while c < len(palavra[0]):     
-            if letra == palavra[0][c]:
+#     else:
+#         while c < len(palavra[0]):     
+#             if letra == palavra[0][c]:
                 
-                if len(letras) == len(palavra[0]):
+#                 if len(letras) == len(palavra[0]):
                     
-                    if letra == palavra[0][c]:
+#                     if letra == palavra[0][c]:
                         
-                            print(letras)
-                            letras.remove(letras[c]) 
-                            letras.insert(c, palavra[0][c])
+#                             print(letras)
+#                             letras.remove(letras[c]) 
+#                             letras.insert(c, palavra[0][c])
                             
-                            print(letras)
+#                             print(letras)
                        
-            c += 1     
+#             c += 1     
             
-    for letra in letras:
-        print(letra, end=' ')
-    print()
+#     for letra in letras:
+#         print(letra, end=' ')
+#     print()
     
     

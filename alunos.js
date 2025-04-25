aa107fb8a20d0abf79276ae6284fc63485058fcd
Satiro07 <jardelsatiro57@gmail.com>
2025-04-25 18:44:23 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -3,40 +3,41 @@ const prompt = require('prompt-sync')();
 let alunos = [];
 function menu() {
     console.log('MENU');
-    console.log(' 1. adicionar aluno\n 2. listar alunos\n 3. buscar aluno\n 4. sair');
+    console.log(' 1. adicionar aluno(a)\n 2. listar alunos\n 3. buscar aluno(a)\n 4. sair');
     let escolha = prompt('Escolha uma opção: ');
     return escolha;
 };
 
 function add_alunos () {
-    let nome = prompt('Nome do aluno: ');
+    let nome = prompt('Nome do aluno(a): ');
     let notas = []
     for (let i = 1; i <= 3; i++) {
-        let nota = parseFloat(prompt(`Nota ${i} do aluno: `));
+        let nota = parseFloat(prompt(`Nota ${i} do aluno(a): `));
         notas.push(nota);
     }
     let media = notas.reduce((acumulador, atual) => acumulador + atual, 0) / 3;
-    console.log(`Aluno ${nome} adicionado com sucesso!`)
+    console.log(`Aluno(a) ${nome} adicionado com sucesso!`)
     alunos.push({nome: nome, notas: notas, media: media.toFixed(2)});
 };
 
 function listar_alunos () {
     for (let aluno of alunos) {
-        console.log(`Aluno: ${aluno['nome']}`);
+        console.log(`Aluno(a): ${aluno['nome']}`);
         let quantidade = aluno['notas'].length 
         for (let i = 0; i < quantidade; i++) {
             console.log(`Nota ${i+1}: ${aluno['notas'][i]}`);
         }
         console.log(`Média: ${aluno['media']}`);
+        console.log();
     }
 };
 
 function buscar_aluno () {
-    let buscar_nome = prompt('Nome do aluno que deseja procurar: ');
+    let buscar_nome = prompt('Nome do aluno(a) que deseja procurar: ');
     let verificacao = false;
     for (let aluno of alunos) {
         if (buscar_nome.toLowerCase() == aluno['nome'].toLowerCase()) {
-            console.log(`Aluno: ${aluno['nome']}`);
+            console.log(`Aluno(a): ${aluno['nome']}`);
             verificacao = true;
             let quantidade = aluno['notas'].length 
             for (let i = 0; i < quantidade; i++) {
@@ -45,7 +46,7 @@ function buscar_aluno () {
         }
     }
     if (verificacao == false) {
-        console.log(`Aluno ${buscar_nome} não encontrado!`);
+        console.log(`Aluno(a) ${buscar_nome} não encontrado!`);
     }
 };
 

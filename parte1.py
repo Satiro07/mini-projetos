337f8c165e392a713b268d7f58ae5507a557b95c
Satiro07 <jardelsatiro57@gmail.com>
2025-04-26 13:21:02 -0300

    .

---
+++ parte1.py
@@ -0,0 +1,24 @@
+import parte2
+categorias = []
+def menu(esc):
+    print('1. adicionar categoria')
+    print('2. adicionar tarefa')
+    print('3. ver tarefas')
+    print('4. sair')
+    escolha = input('Escolha uma opção: ')
+    return escolha
+
+
+while True:
+    esc = menu(0)
+    if esc == '1':
+        parte2.adicionar_categorias()
+    elif esc == '2':
+        parte2.adicionar_tarefas()
+    elif esc == '3':
+        parte2.exibir_tarefas()
+    elif esc == '4':
+        print('Saindo')
+        break
+    else:
+        print('Opção invalida')
--- parte2.py
+++ parte2.py
@@ -1,24 +1,24 @@
 
-def adicionar_categorias():
+def adicionar_categorias(lista):
     esc = 's'
     while esc != 'n':
         nome_escolha_categoria = input('Nome da categoria: ')
-        if nome_escolha_categoria not in categorias:
-            categorias.append([nome_escolha_categoria, []])
+        if nome_escolha_categoria not in lista:
+            lista.append([nome_escolha_categoria, []])
             print(f'Categoria {nome_escolha_categoria} adicionada com sucesso!')
         else:
             print('Categoria já esta adicionada!')
-        esc = input('Deseja adicionar mais categorias? [s/n] ').lower()
+        esc = input('Deseja adicionar mais lista? [s/n] ').lower()
 
-def adicionar_tarefas():
+def adicionar_tarefas(lista):
     verificacao = False
-    if not categorias:
+    if not lista:
         print('Nenhuma categoria adicionada! Você precisa adicionar pelo menos uma!')
     else:
-        for categoria in categorias:
+        for categoria in lista:
             print(f'Nome da categoria: {categoria[0]}')
         nome_escolha_categoria = input('Nome da categoria que deseja adicionar : ').lower()
-        for categoria in categorias:
+        for categoria in lista:
             if nome_escolha_categoria == categoria[0].lower():
                 verificacao = True
                 escolha = 's'
@@ -34,21 +34,21 @@ def adicionar_tarefas():
         opcao = input('Deseja adicionar ela? [s/n] ').lower()
         if opcao == 's':
             nome_categoria = input('Digito o nome da categoria que quer adicionar: ')
-            categorias.append([nome_categoria, []])
+            lista.append([nome_categoria, []])
             print(f'Categoria {nome_categoria} adicionada com sucesso!')
             adicionar_tarefas()
   
-def exibir_tarefas():
+def exibir_tarefas(lista):
     verificacao = False
-    if not categorias:
+    if not lista:
         print('Nenhuma categoria adicionada! Você precisa adicionar pelo menos uma!')
     else: 
         cont = 0
-        for categoria in categorias:
+        for categoria in lista:
             if categoria[1] != []:
                 verificacao = True
                 if cont == 0: 
-                    print('Categorias e tarefas adicionadas: ')
+                    print('lista e tarefas adicionadas: ')
                     cont += 1
                 print(f'Nome da categoria: {categoria[0]}')
                 print('Tarefas: ')

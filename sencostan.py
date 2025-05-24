43b4cace4505dc4d141a8d3b2cbc3526873c4644
Satiro07 <jardelsatiro57@gmail.com>
2025-05-23 21:20:57 -0300

    Update sencostan.py

--- sencostan.py
+++ sencostan.py
@@ -1,5 +1,16 @@
+from math import cos
 def seno(angulo):
-    rad = angulo * (3.14159/180)
-    return rad
+    seno = angulo * (3.14159/180)
+    return seno
 
 print(seno(30))
+
+
+def cosseno(angulo):
+    x = angulo * (3.14159/180)
+    cosseno = 1 - ((x**2/2) + (x**4/24))
+    return cosseno
+
+print(cosseno(30))
+
+print(cos(0.523598))
